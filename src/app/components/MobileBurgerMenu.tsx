4e7b8fecdb0e6d34ebfc14df25d1e4d2f57aa580
George Townley <george@knowlesavenue.com>
2025-07-05 15:10:43 +0100

    update burger menu on mobile to slide all the way out

--- src/app/components/MobileBurgerMenu.tsx
+++ src/app/components/MobileBurgerMenu.tsx
@@ -40,7 +40,7 @@ export default function MobileBurgerMenu({
 
       {/* Slide-out Menu */}
       <div
-        className={`fixed top-0 right-0 h-full w-full max-w-sm z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-full z-50 transform transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
         style={{
@@ -89,14 +89,6 @@ export default function MobileBurgerMenu({
           ))}
         </div>
       </div>
-
-      {/* Backdrop */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300"
-          onClick={closeMenu}
-        />
-      )}
     </div>
   );
 }
